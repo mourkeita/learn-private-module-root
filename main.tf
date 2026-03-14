abd5f0a523edeef5f730b951fc406d4bfdeca0f4
mourkeita <mour.keita@gmail.com>
2026-03-14 02:42:33 +0100

    Update workspace name and app name

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name    = var.name
-  region  = var.region
-  prefix  = var.prefix
+  source  = "app.terraform.io/learn-terraform-mk/s3-webapp-custom-mour/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
   version = "1.0.0"
 }
